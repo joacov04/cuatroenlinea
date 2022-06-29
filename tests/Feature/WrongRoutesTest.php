20cbc7303eb414fe45f5a0b93c13f015dd9e83f6
Joaquin <joaquinvergara070@gmail.com>
2022-06-29 11:45:01 -0300

    added tests for wrong routes

---
+++ tests/Feature/WrongRoutesTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class WrongRoutesTest extends TestCase
+{
+    public function test_invalid_number_left()
+    {
+        $response = $this->get('/jugar/0');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_invalid_number_right()
+    {
+        $response = $this->get('/jugar/8');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_char_in_route()
+    {
+        $response = $this->get('/jugar/a');
+
+        $response->assertStatus(200);
+    }
+}
