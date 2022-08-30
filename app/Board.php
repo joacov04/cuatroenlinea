4422603f7549d0de16ca0d3b45081b01ca9c921c
Joaquin <joaquinvergara070@gmail.com>
2022-08-30 13:40:10 -0300

    wip: implemented some board functions

--- app/Board.php
+++ app/Board.php
@@ -6,7 +6,63 @@ interface BoardInterface {
     public function putPiece(Piece $piece, int $column) : bool;
     public function getPiece(int $column, int $row) : ?Piece;
     public function cleanBoard() : void;
+    public function getRowsCount() : int;
+    public function getColumnsCount() : int;
 
+}
+
+class Board implements BoardInterface {
+    protected $columns;
+    protected int $columns_count, $rows_count;
+
+    public function __construct(int $columns_count = 7, int $rows_count = 8) {
+        if($columns_count < 4 || $rows_count < 4) {
+            throw new \Exception("Rows and columns counts must be >= 4");
+        }
+        $this->columns_count = $columns_count;
+        $this->rows_count = $rows_count;
+
+    }
+
+    public function getRowsCount() : int {
+        return $this->rows_count;
+    }
+
+    public function getColumnsCount() :int {
+        return $this->columns_count;
+    }
+
+    public function putPiece(Piece $piece, int $column) : bool {
+        $column -= 1;
+        if($column >= $this->getColumnsCount() || $column < 0) {
+            throw new \Exception("Column out of range.");
+        }
+
+        if(count($this->columns[$column]) >= $this->getRowsCount()) {
+            return false;
+        }
+
+        $this->columns[$column][] = $piece;
+        return true;
+
+    }
+    // ⬜
+    
+    public function getPiece(int $column, int $row) : ?Piece {
+        $column -= 1;
+        $row -= 1;
+        $out_left = ($column < 0 || $row < 0);
+        $out_right = ($column > $this->getColumnsCount() || $row > $this->getRowsCount());
+        if($out_left || $out_right) {
+            throw new \Exception("Index('es) out of range.");
+        }
+        return $this->columns[$column][$row];
+
+    }
+
+    public function cleanBoard() : void {
+
+    }
 }
 
 
--- app/Piece.php
+++ app/Piece.php
@@ -3,10 +3,20 @@
 namespace App;
 
 interface PieceInterface {
-
+    public function getColor() : string;
 }
 
 class Piece implements PieceInterface {
+    protected $color;
+
+    public function __construct($color) {
+        // 🟥🟦
+        $this->color = $color;
+    }
+
+    public function getColor() : string {
+        return $this->color;
+    }
 
 }
 
